07bd085f9a2e7758275264292fa4e71ec7086190
opex <opex@opex-Aspire-5538G>
2018-10-01 21:26:49 +0700

    testing commit

--- Src/main.c
+++ Src/main.c
@@ -638,6 +638,7 @@ void GPSHadlerFunc(void const * argument)
 		dysplayBufferStruct SymBuffer;
 		xStatus=xQueueReceive(GPSHandlerHandle, &gpsData, 1);
 		if (xStatus == pdPASS){
+			//Проверочный коммит
 			//Сдвигаем буфер с замерами
 			for (uint8_t var = 0;  var < 4; ++var) {
 				messageArray[var]=messageArray[var+1];
